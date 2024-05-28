f0c4f354033341496ea1f7477a3d844c122c20e9
calvinho43 <guibrittotava@gmail.com>
2024-05-28 15:49:44 -0300

    Teste

---
+++ GestorDeEstudantesT6-main/GestorDeEstudantesT6/Estudante.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeEstudantesT6
+{
+    internal class Estudante
+    {
+        MeuBancoDeDados meuBancoDeDados = new MeuBancoDeDados();
+        public bool inserirEstudante(string nome, string sobrenome, DateTime nascimento, string genero, string telefone, 
+            string endereco, MemoryStream foto)
+        {
+            MySqlCommand comando = new MySqlCommand("INSERT INTO `estudantes`(`nome`, `sobrenome`, `nascimento`, `genero`, `telefone`, `endereco`, `foto`) VALUES (@nome,@sobrenome,@nascimento,@genero,@telefone,@endereco,@foto,[value-8])", meuBancoDeDados.getConexao);
+            comando.Parameters.Add("@nome", MySqlDbType.VarChar).Value = nome;
+            comando.Parameters.Add("@sobrenome", MySqlDbType.VarChar).Value = sobrenome;
+            comando.Parameters.Add("@nascimento", MySqlDbType.Date).Value = nascimento;
+            comando.Parameters.Add("@genero", MySqlDbType.VarChar).Value = genero;
+            comando.Parameters.Add("@telefone", MySqlDbType.VarChar).Value = telefone;
+            comando.Parameters.Add("@endereco", MySqlDbType.Text).Value = endereco;
+            comando.Parameters.Add("@foto", MySqlDbType.LongBlob).Value = foto;
+            return true;
+        }
+    }
+}
---
+++ GestorDeEstudantesT6-main/GestorDeEstudantesT6/FormInseirEstudante.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace GestorDeEstudantesT6
+{
+    partial class FormInseirEstudante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxSobrenome = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBoxGenero = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.Feminino = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTelefone = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxEndereco = new System.Windows.Forms.TextBox();
+            this.pictureBoxUsuario = new System.Windows.Forms.PictureBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.groupBoxGenero.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUsuario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(106, 29);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(163, 20);
+            this.textBoxNome.TabIndex = 5;
+            // 
+            // textBoxSobrenome
+            // 
+            this.textBoxSobrenome.Location = new System.Drawing.Point(106, 75);
+            this.textBoxSobrenome.Name = "textBoxSobrenome";
+            this.textBoxSobrenome.Size = new System.Drawing.Size(163, 20);
+            this.textBoxSobrenome.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(87, 121);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Sobrenome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Gênero";
+            // 
+            // groupBoxGenero
+            // 
+            this.groupBoxGenero.Controls.Add(this.radioButton2);
+            this.groupBoxGenero.Controls.Add(this.Feminino);
+            this.groupBoxGenero.Location = new System.Drawing.Point(106, 184);
+            this.groupBoxGenero.Name = "groupBoxGenero";
+            this.groupBoxGenero.Size = new System.Drawing.Size(200, 66);
+            this.groupBoxGenero.TabIndex = 13;
+            this.groupBoxGenero.TabStop = false;
+            this.groupBoxGenero.Text = "groupBox1";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(108, 33);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Masculino";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Feminino
+            // 
+            this.Feminino.AutoSize = true;
+            this.Feminino.Location = new System.Drawing.Point(19, 33);
+            this.Feminino.Name = "Feminino";
+            this.Feminino.Size = new System.Drawing.Size(67, 17);
+            this.Feminino.TabIndex = 0;
+            this.Feminino.TabStop = true;
+            this.Feminino.Text = "Feminino";
+            this.Feminino.UseVisualStyleBackColor = true;
+            this.Feminino.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Telefone";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBoxTelefone
+            // 
+            this.textBoxTelefone.Location = new System.Drawing.Point(106, 300);
+            this.textBoxTelefone.Name = "textBoxTelefone";
+            this.textBoxTelefone.Size = new System.Drawing.Size(163, 20);
+            this.textBoxTelefone.TabIndex = 15;
+            this.textBoxTelefone.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 354);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Endereço";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBoxEndereco
+            // 
+            this.textBoxEndereco.Location = new System.Drawing.Point(106, 354);
+            this.textBoxEndereco.Name = "textBoxEndereco";
+            this.textBoxEndereco.Size = new System.Drawing.Size(163, 20);
+            this.textBoxEndereco.TabIndex = 17;
+            // 
+            // pictureBoxUsuario
+            // 
+            this.pictureBoxUsuario.Image = global::GestorDeEstudantesT6.Properties.Resources.Personagem_sprite1;
+            this.pictureBoxUsuario.Location = new System.Drawing.Point(87, 403);
+            this.pictureBoxUsuario.Name = "pictureBoxUsuario";
+            this.pictureBoxUsuario.Size = new System.Drawing.Size(122, 111);
+            this.pictureBoxUsuario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxUsuario.TabIndex = 18;
+            this.pictureBoxUsuario.TabStop = false;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.Location = new System.Drawing.Point(3, 564);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(135, 51);
+            this.buttonLogin.TabIndex = 19;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancelar.Location = new System.Drawing.Point(162, 564);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(135, 51);
+            this.buttonCancelar.TabIndex = 20;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // FormInseirEstudante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 664);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.pictureBoxUsuario);
+            this.Controls.Add(this.textBoxEndereco);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxTelefone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBoxGenero);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBoxSobrenome);
+            this.Controls.Add(this.textBoxNome);
+            this.Name = "FormInseirEstudante";
+            this.Text = "FormInseirEstudante";
+            this.groupBoxGenero.ResumeLayout(false);
+            this.groupBoxGenero.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUsuario)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxSobrenome;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBoxGenero;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton Feminino;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTelefone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxEndereco;
+        private System.Windows.Forms.PictureBox pictureBoxUsuario;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonCancelar;
+    }
+}
